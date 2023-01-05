7748373e6819dc159e32d21be75446fe1e8fdbfa
Luis Cardona <cardona-luis@outlook.com>
2023-01-05 08:52:10 -0600

    Subscription

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import ActiveWebsite from './components/ActiveWebsite';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>LC Website Solutions</p>
         <h1>The Hook Up BBQ</h1>
+        <ActiveWebsite></ActiveWebsite>
+        <p>by Luis Cardona</p>
       </header>
     </div>
   );
---
+++ client/src/components/ActiveWebsite.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ActiveWebsite(props) {
+  return (
+    
+    <div className='sub-container'>
+
+      
+    <button>
+    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8XQJR2M39U7FQ">Activate/Subscribe</a>
+    </button>
+    <img src="https://www.paypalobjects.com/digitalassets/c/website/marketing/na/us/logo-center/9_bdg_secured_by_pp_2line.png" border="0" alt="Secured by PayPal" />
+</div>
+  )
+}
+
+
+export default ActiveWebsite
