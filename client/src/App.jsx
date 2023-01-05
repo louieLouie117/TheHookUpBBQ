84d5bcb4da41b58e0113c602d790bb1e12938602
Luis Cardona <cardona-luis@outlook.com>
2023-01-05 08:09:15 -0600

    first-commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The Hook Up BBQ</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
